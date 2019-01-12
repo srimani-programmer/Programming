3f4c714f36440c4c4c8d42e0a435f5e62c71e355
srimani-programmer <srimani.scholar@gmail.com>
2019-01-12 18:13:22 +0530

    Final Commit to all files of R and Python Programming.

---
+++ R-Files/10DaysOfStatistics/Challenge0.R
@@ -0,0 +1,15 @@
+vtr = c()
+
+vtr <- scan()
+
+meanValue = mean(vtr)
+
+medianValue = median(vtr)
+
+modeValue = mode(vtr)
+
+print(meanValue)
+
+print(medianValue)
+
+print(modeValue)
---
+++ R-Files/BasicMathematicalOperations.R
@@ -0,0 +1,25 @@
+# Basic Arthimetic Operations
+
+print(2 + 3 + 7)
+
+print(2-1+7-5)
+
+print(2*4*7*8)
+
+print(4/2.0)
+
+print(4.3/2)
+
+print(0/0)
+
+print(0.0/3)
+
+print(0.00005430000/0.323323)
+
+print(123/0)
+
+print(132/-0.0)
+
+print(2^10)
+
+print(5^7)
